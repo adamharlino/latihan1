878d182b60e1f9a6cd0c65bf46da349581d8f59a
adamharlino <adamharlino@gmail.com>
2026-04-09 21:29:02 +0700

    commit ke github

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:latihan1/hello_world.dart';
-//import 'package:latihan1/column_widget.dart';
-//import 'package:latihan1/row_widget.dart';
+import 'package:latihan1/hello_world.dart';
+import 'package:latihan1/column_widget.dart';
+import 'package:latihan1/row_widget.dart';
 import 'package:latihan1/ui/produk_form.dart';
 
 void main() {
