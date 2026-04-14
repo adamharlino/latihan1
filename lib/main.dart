ab1afd625f1fc721e6e3dc551aeb4932f3b25d2c
adamharlino <adamharlino@gmail.com>
2026-04-14 13:49:47 +0700

    commit kedua

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:latihan1/hello_world.dart';
 import 'package:latihan1/column_widget.dart';
 import 'package:latihan1/row_widget.dart';
 import 'package:latihan1/ui/produk_form.dart';
-import 'package:simple_alert_dialog/simple_alert_dialog.dart';
-import 'package:cherry_toast/cherry_toast.dart';
 
 void main() {
   runApp(MyApp());
